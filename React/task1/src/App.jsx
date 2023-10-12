db7775ff8a8dca718f4341753af74bd074a9e85c
thumeshsai <92836438+thumeshsai@users.noreply.github.com>
2023-10-12 12:27:50 +0530

    Add files via upload

---
+++ React/task1/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='border p-4'>
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App
